81d8f517bf3905eb55093b48301ea9336c4e3793
ringeringeraja <joaosan177@gmail.com>
2024-02-03 05:27:32 -0300

    feat(create-aeria-app): implement checks

--- packages/create-aeria-app/src/cli.ts
+++ packages/create-aeria-app/src/cli.ts
@@ -1,7 +1,17 @@
 import path from 'path'
 import { execSync } from 'child_process'
 import { parseArgs } from 'util'
+import semver from 'semver'
 import fs from 'fs/promises'
+
+type Tuple = [string] | [undefined, string]
+
+const error = (value: string): Tuple => [value]
+const success = (value: string): Tuple => [,value]
+const isError = (tuple: Tuple) => !!tuple[1]
+const unwrap = (tuple: Tuple) => tuple[0] || tuple[1]
+
+const $ = (cmd: string) => execSync(cmd).toString().trim()
 
 const {
   positionals,
@@ -20,26 +30,52 @@ const DEFAULTS = {
   template: 'ts'
 }
 
+const allChecksPass = async (checks: Promise<Tuple>[]) => {
+  for( const check of checks ) {
+    const result = await check
+    if( isError(result) ) {
+      return
+    }
+  }
+
+  return true
+}
+
 const checkPackageVersion = async () => {
   const {
     name: packageName,
     version: packageVersion
   } = require('../package.json')
 
-  const remoteVersion = execSync(`npm view ${packageName} version`).toString().trim()
+  const remoteVersion = $(`npm view ${packageName} version`)
 
   if( packageVersion !== remoteVersion ) {
-    console.log('local and remote versions differ')
+    return error('local and remote versions of this package differ')
   }
 
-  console.log({
-    remoteVersion,
-    packageVersion
-  })
+  return success('ok')
+}
+
+const checkCompatibility = async () => {
+  const localNodeVersion = $('node -v')
+  const remoteNodeVersion = $('npm view aeria engine.node')
+
+  if( !semver.satisfies(localNodeVersion, remoteNodeVersion) ) {
+    return error('local node version is outdated')
+  }
+
+  return success('')
 }
 
 const main = async () => {
-  await checkPackageVersion()
+  const checksOk = await allChecksPass([
+    checkPackageVersion(),
+    checkCompatibility()
+  ])
+
+  if( !checksOk ) {
+    return
+  }
 
   const cwd = process.cwd()
   const projectPath = path.join(cwd, positionals[0])
