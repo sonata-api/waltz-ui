f4a582db8a8cdc148e5bc8b501e7cfce4580971c
ringeringeraja <joaosan177@gmail.com>
2024-02-03 04:48:46 -0300

    feat(create-aeria-app): initial implementation

--- packages/create-aeria-app/src/cli.ts
+++ packages/create-aeria-app/src/cli.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import { execSync } from 'child_process'
 import { parseArgs } from 'util'
 import fs from 'fs/promises'
 
@@ -19,11 +20,27 @@ const DEFAULTS = {
   template: 'ts'
 }
 
-const moveFiles = () => {
-  //
+const checkPackageVersion = async () => {
+  const {
+    name: packageName,
+    version: packageVersion
+  } = require('../package.json')
+
+  const remoteVersion = execSync(`npm view ${packageName} version`).toString().trim()
+
+  if( packageVersion !== remoteVersion ) {
+    console.log('local and remote versions differ')
+  }
+
+  console.log({
+    remoteVersion,
+    packageVersion
+  })
 }
 
 const main = async () => {
+  await checkPackageVersion()
+
   const cwd = process.cwd()
   const projectPath = path.join(cwd, positionals[0])
 
